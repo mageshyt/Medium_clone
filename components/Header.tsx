1f2163235e9a6d97a76cd17fb36167256c62dd5f
magesh <hariarunengg@gmail.com>
2022-04-23 17:39:21 +0530

    sanity added

---
+++ components/Header.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+import React from 'react'
+const Header = () => {
+  return (
+    <header className="mx-auto flex max-w-7xl items-center justify-between p-6">
+      {/* Left side Logo */}
+      <div className="flex items-center space-x-5">
+        <Link href="/">
+          <img
+            className="w-44 cursor-pointer object-contain "
+            src="https://links.papareact.com/yvf"
+            alt="logo"
+          />
+        </Link>
+
+        <div className="hidden items-center space-x-4 md:flex">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className="rounded-2xl bg-green-600 px-4 py-1 text-white">
+            follow
+          </h3>
+        </div>
+      </div>
+      {/* Right */}
+      <div className="flex cursor-pointer items-center space-x-4 font-medium text-green-600  ">
+        <span>Sign In</span>
+        <span className="cursor-pointer rounded-3xl border border-green-500 px-4 py-1">
+          Get Started
+        </span>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import 'primereact/resources/themes/lara-light-indigo/theme.css' //theme
+import 'primereact/resources/primereact.min.css' //core css
+import 'primeicons/primeicons.css' //icons
+
+import PrimeReact from 'primereact/api'
+
+PrimeReact.ripple = true
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
